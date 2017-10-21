c83a456b653bb90aced7962565e476ad1fc340ec
irabeson <IohannRabeson@users.noreply.github.com>
2017-10-21 18:53:24 -0400

    [Recorder]: fixed crash when the file selected does not have extension.

--- utils/Path.cpp
+++ utils/Path.cpp
@@ -1,5 +1,7 @@
 #include "Path.hpp"
 
+#include <iostream>
+
 std::string Path::extractFileName(std::string const& path)
 {
 	auto lastSlashPos = path.find_last_of("\\/");
@@ -45,6 +47,10 @@ std::string Path::extractExtension(std::string const& path)
 	if (lastDotPos != std::string::npos)
 	{
 		lastDotPos += 1;
+	}
+	else
+	{
+		lastDotPos = path.size();
 	}
 	return path.substr(lastDotPos);
 }
