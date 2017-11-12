f00eba79bef0630c3a6444775df996312a3bc281
irabeson <iohannrabeson@gmail.com>
2017-11-11 23:15:43 -0500

    Make clock available.

--- src/Simple.cpp
+++ src/Simple.cpp
@@ -15,5 +15,5 @@ void init(rack::Plugin *p)
 	p->addModel(rack::createModel<ButtonTriggerWidget>("IO-Simple", "Simple", "IO-ButtonTrigger", "Button Trigger"));
 	p->addModel(rack::createModel<ClockDividerWidget>("IO-Simple", "Simple", "IO-ClockDivider", "Clock Divider"));
 	p->addModel(rack::createModel<RecorderWidget>("IO-Simple", "Simple", "IO-Recorder", "Recorder"));
-//	p->addModel(rack::createModel<ClockWidget>("IO-Simple", "Simple", "IO-Clock", "Clock"));
+	p->addModel(rack::createModel<ClockWidget>("IO-Simple", "Simple", "IO-Clock", "Clock"));
 }
